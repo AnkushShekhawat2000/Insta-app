57b7b6fe470ba060c47e571f60dced3873359946
Ankush Shekhawat <ankush75056@gmail.com>
2024-01-25 22:17:34 +0530

    insta-1

---
+++ src/Components/Dashboard.js
@@ -0,0 +1,47 @@
+import React, { useState } from "react"
+import axios from "axios"
+
+
+const Dashboard =({token})=>{
+
+    const [message, setMessage] = useState("");
+
+ 
+   async function getJoke(){
+         try{
+            const response = await axios.get("https://instagram-express-app.vercel.app/api/auth/zuku",
+            {
+                headers:{
+                    authorization : `Bearer ${token}`
+                }
+            })
+            // console.log("dashboard")
+
+            setMessage(response.data.data.message)
+         }
+         catch(err){
+
+            // console.log("fail ho gya",err)
+
+
+
+         }
+
+    }
+
+    return (
+        <div>
+            <h1>Dashboard</h1>
+            {
+                message && <h2>{message}</h2>
+            }
+            <button onClick={getJoke}>Get Job</button>
+        </div>
+    )
+}
+
+export default Dashboard;
+
+
+
+// logout api
---
+++ src/Components/Signup.js
@@ -0,0 +1,189 @@
+// import React, {useState} from "react"
+// import axios from "axios"
+
+
+// const Signup = ()=>{
+
+//     const [user, setUser] = useState({
+//         name:"",
+//         email:"",
+//         password:"",
+//         confirmPassword:""
+
+//         // name,email,password,confirmpassword
+//     });
+
+//     const [successMessage, setSuccessMessage] = useState("");
+//     const [errorMessage, setErrorMessage] = useState("");
+
+//     const {name, email, password, confirmPassword} = user;
+
+//     function updateUser(e){
+//       e.preventDefault();
+//         let key = e.target.name;
+//         let value = e.target.value;
+//         setUser({...user, [key]:value})
+        
+//     }
+
+//    async function implementSignup(e){
+//     // e.pereventDefault();
+
+//     try{
+//         const response = await axios.post("https://instagram-express-app.vercel.app/api/auth/signup",
+//         {
+//             name : name,
+//             "email": email,
+//             "password": password,
+  
+//         })
+ 
+//         console.log("suscess", response.data);
+
+//         setSuccessMessage(response.data.message);
+//     }
+//    catch(err){
+//     console.log("failure", err)
+//     setErrorMessage(err.response.data.message);
+//    }
+
+// }
+
+//     return (
+//         <div>
+//             {
+//                 successMessage && <h2>{successMessage}</h2>
+//             }
+
+//             {
+//                 errorMessage && <h2>{errorMessage}</h2>
+//             }
+
+//            <form onSubmit={implementSignup}>
+
+//                 <input type="text" placeholder="Enter Name" name="name"
+//                     value={name}
+//                     onChange={updateUser}
+//                 />
+//                 <input type="text" placeholder="Enter Email" name="email"
+//                     value={email}
+//                     onChange={updateUser}
+//                 />
+//                 <input type="text" placeholder="Enter password" name="password"
+//                     value={password}
+//                     onChange={updateUser}
+//                 />
+//                 <input type="text" placeholder="Enter your confirm Password" name="confirmPassword"
+//                     value={confirmPassword}
+//                     onChange={updateUser}
+//                 />
+//                 <button type="submit">Submit</button>
+//            </form>    
+//         </div>
+//     )
+// }
+
+// export default Signup;
+
+
+
+import React,{useState} from "react";
+import axios from "axios";
+
+const Signup = ({setToken}) => {
+    const [user, setUser] = useState({
+        name:"",
+        email:"",
+        password:"",
+        confirmPassword:""
+    });
+
+    const [successMessage, setSuccessMessage] = useState("");
+    const [errorMessage, setErrorMessage] = useState("");
+
+   
+    
+    const {name, email, password, confirmPassword} = user;
+
+
+    function updateUser(e){
+        let key = e.target.name;
+        let value = e.target.value;
+        setUser({...user, [key]:value})
+
+    }
+
+    async function implementSignup(e){
+        e.preventDefault();
+
+
+        // valiations
+
+
+        try{
+
+        const response =  await axios.post("https://instagram-express-app.vercel.app/api/auth/signup",
+            {
+                name: name,
+                "email": email,
+                "password": password,
+            })
+            console.log("sucess", response)
+            console.log("Success", response.data)
+            setSuccessMessage(response.data.message)
+            setToken();
+            setToken(response.data.data.token)
+            setErrorMessage("")
+            setUser({
+                name:"",
+                email:"",
+                password:"",
+                confirmPassword:""
+
+            })
+
+        }
+
+        catch(err){
+            console.log("Failure", err)
+            setErrorMessage(err.response.data.message)
+            setSuccessMessage("")
+        }
+
+    }
+
+    return(
+        <div> 
+            <h1>Signup</h1>
+            {
+                successMessage && <h2>{successMessage}</h2>
+            }
+            {
+                errorMessage && <h2>{errorMessage}</h2>
+            }
+
+              <form onSubmit={ implementSignup}>
+                  <input type="text" placeholder="Enter Name"  name="name"
+                   value = {name}
+                     onChange = {updateUser}
+                  />
+                   <input type="email" placeholder="Enter Email"  name="email"
+                   value = {email}
+                     onChange = {updateUser}
+                  />
+                   <input type="password" placeholder="Enter Password"  name="password"
+                   value = {password}
+                     onChange = {updateUser}
+                  />
+                   <input type="password" placeholder="Confirm Password"  name="confirmPassword"
+                   value = {confirmPassword}
+                     onChange = {updateUser}
+                  />
+                    <button type="submit">Submit</button>
+              </form>
+              <hr />
+        </div>
+    )
+}
+
+export default Signup;
